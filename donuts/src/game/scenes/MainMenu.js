5190e29182cfbd14763f474798fbdcf07dfd677c
Gsbirds <gabbyburgard@yahoo.com>
2024-10-26 02:08:41 -0600

    changes to menu load

--- donuts/src/game/scenes/MainMenu.js
+++ donuts/src/game/scenes/MainMenu.js
@@ -34,7 +34,7 @@ export class MainMenu extends Scene {
     handlePathChange(currentPath, isPageRefreshed) {
         if (currentPath.includes('home') || currentPath === 'https://gsbirds.github.io/donut_portfolio/#/home' || currentPath === 'https://gsbirds.github.io/donut_portfolio/') {
             localStorage.removeItem('donutClicked');
-            EventBus.emit('home-menu-clicked', true);
+            EventBus.emit('home-menu-clicked', false);
             if (isPageRefreshed) {
                 localStorage.removeItem('donutClicked');
                 EventBus.emit('home-menu-clicked', false);
