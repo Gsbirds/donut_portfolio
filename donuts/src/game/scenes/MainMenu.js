f220e5f7aeacf6014a619ca8fa846e91f50bfb5e
Gsbirds <gabbyburgard@yahoo.com>
2024-11-24 07:05:38 -0700

    back

--- donuts/src/game/scenes/MainMenu.js
+++ donuts/src/game/scenes/MainMenu.js
@@ -211,18 +211,7 @@ export class MainMenu extends Scene {
                     });
                     localStorage.removeItem('donutClicked');
                     EventBus.emit('home-menu-clicked', false);
-                    const isHomePage = currentPath.includes('home') || currentPath === 'https://gsbirds.github.io/donut_portfolio/#/home';
-                    const isMobile = window.innerWidth <= 768;
-                
-                    if (isHomePage && isMobile) {
-                        const gameContainer = document.getElementById('game-container');
-                        if (gameContainer) {
-                            gameContainer.style.top = '24%';
-                            gameContainer.style.left = '54%';
-                            gameContainer.style.right = '90%';
-                            gameContainer.style.width = '140%';
-                        }
-                    }
+                    
 
                     }
                     url = `${window.location.origin}/donut_portfolio/${donutLinks[i].toLowerCase()}`;
